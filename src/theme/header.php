66a03f2fd6175799d9a2a0f6d1ece5a9dcd2ba01
George Reynolds <primopinto@gmail.com>
2018-09-19 18:26:51 -0500

    Added specials to the full screen nav and put GA & Facebook pixel back into template headers.

--- src/theme/header.php
+++ src/theme/header.php
@@ -6,6 +6,31 @@
 	<?php wp_head(); ?>
 	<link type="text/css" rel="stylesheet" href="//fast.fonts.net/cssapi/8bca4bb0-90b5-40cd-9646-32cd405f6aa4.css"/>
 	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico" type="image/x-icon">
+	<!-- Google Analytics -->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-27114347-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+	<!-- End Google Analytics -->
+
+	<!-- Facebook Pixel Code -->
+	<script>
+		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+		n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+		document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+		fbq('init', '205064539829223');
+		fbq('track', "PageView");
+	</script>
+	<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=205064539829223&ev=PageView&noscript=1"/></noscript>
+	<!-- End Facebook Pixel Code -->
 </head>
 
 <body <?php body_class(); ?>>
